39e55f2ea3d617452b76ffcf0a6e963cd9d01692
Adrianna <adriannakoszowska@gmail.com>
2015-02-28 20:55:23 +0100

    Wgranie TidyCMS

---
+++ tidycms/server.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("opcache.enable", "0");
+ini_set("xcache.cacher", "0");
+ini_set("eaccelerator.enable", "0");
+ini_set("detect_unicode", "0");
+$version = "master.159-f5e4aa1.2015-01-08";
+
+require_once('tidycms-connector.phar');
+
+?>
